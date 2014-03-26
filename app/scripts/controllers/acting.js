946e2617f02167d474ae819bc2a5db4e7595fb85
ackushiw <ackushiw@users.noreply.github.com>
2014-03-26 02:24:40 -0400

    added resume accordion

--- app/scripts/controllers/acting.js
+++ app/scripts/controllers/acting.js
@@ -2,15 +2,206 @@
 
 angular.module('ackushiwApp')
     .controller('ActingCtrl', function($scope) {
-        $scope.oneAtATime = true;
+        $scope.accordion = true;
 
-        $scope.groups = [{
-            title: 'Dynamic Group Header - 1',
-            content: 'Dynamic Group Body - 1'
+        $scope.stats = {
+            eyes: 'Dark Hazel',
+            hair: 'Dark Brown',
+            height: '5\' 10"',
+            weight: '145lb'
+        };
+
+        $scope.theater = [
+        {
+            title: 'Bazaar Noir',
+            character: 'The Riddler',
+            director: 'Jesse Patch, Dir'
+        },  {
+            title: 'Edward Scissorhands',
+            character: 'Edward',
+            director: 'Richard Crawford'
+        },  {
+            title: 'Double Falsehood',
+            character: 'Julio',
+            director: 'Ron Song Destro'
         }, {
-            title: 'Dynamic Group Header - 2',
-            content: 'Dynamic Group Body - 2'
+            title: 'The Comedy of Errors',
+            character: 'Antipholus of Ephesus',
+            director: 'Ron Song Destro'
+        }, {
+            title: 'Love\'s Labour\'s Lost',
+            character: 'Berowne',
+            director: 'Ron Song Destro'
+        }, {
+            title: 'Macbeth',
+            character: 'Malcolm',
+            director: 'Vasile Nedelcu'
+        },  {
+            title: 'The Taming of the Shrew',
+            character: 'Tranio',
+            director: 'Danielle Gaillemain'
+        },  {
+            title: 'Romeo and Juliet',
+            character: 'Romeo',
+            director: 'Sven Saar'
         }];
+
+        $scope.film = [
+        {
+            title: 'Stats',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Theater',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Film',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Commercial',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Conservatory Theater, AADA NYC',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Training',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Skills',
+            character: 'test',
+            director: 'testdir'
+        }];
+
+        $scope.commercial = [
+        {
+            title: 'Stats',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Theater',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Film',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Commercial',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Conservatory Theater, AADA NYC',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Training',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Skills',
+            character: 'test',
+            director: 'testdir'
+        }];
+
+        $scope.aada = [
+        {
+            title: 'Stats',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Theater',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Film',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Commercial',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Conservatory Theater, AADA NYC',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Training',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Skills',
+            character: 'test',
+            director: 'testdir'
+        }];
+
+        $scope.training = [
+        {
+            title: 'Stats',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Theater',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Film',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Commercial',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Conservatory Theater, AADA NYC',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Training',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Skills',
+            character: 'test',
+            director: 'testdir'
+        }];
+
+        $scope.skills = [
+        {
+            title: 'Stats',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Theater',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Film',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Commercial',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Conservatory Theater, AADA NYC',
+            character: 'test',
+            director: 'testdir'
+        }, {
+            title: 'Training',
+            character: 'test',
+            director: 'testdir'
+        },  {
+            title: 'Skills',
+            character: 'test',
+            director: 'testdir'
+        }];
+
+
 
         $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
