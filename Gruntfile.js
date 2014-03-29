347890d9945b7c9b9b44a6b330735e831aa918b4
ackushiw <ackushiw@users.noreply.github.com>
2014-03-29 15:19:58 -0400

    styled for mobile and changed grunt file -bower-install

--- Gruntfile.js
+++ Gruntfile.js
@@ -361,7 +361,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
-      'bower-install',
+      //'bower-install',
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
