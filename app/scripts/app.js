e6cc8428df5ad4fb201c639cc56654cde9a6af9c
ackushiw <ackushiw@users.noreply.github.com>
2014-08-29 01:37:39 -0400

    updated grunt and working out github page hosting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,49 +1,54 @@
 'use strict';
 
-angular.module('ackushiwApp', [
-    'ngCookies',
-    'ngMaterial',
-    'ngResource',
-    'ngSanitize',
-    'ui',
-    'ui.bootstrap',
-    'ui.router',
-    'akoenig.deckgrid',
-    'ngTouch',
-    'ngAnimate'
-
-  ])
-  .config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/alexander');
-    $stateProvider
-      .state('alexander', {
-        url: '/alexander',
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .state('acting', {
-        url: '/acting',
-        templateUrl: 'views/acting.html',
-        controller: 'ActingCtrl'
-      })
-      .state('art', {
-        url: '/art',
-        templateUrl: 'views/art.html',
-        controller: 'ArtCtrl'
-      })
-      .state('art.photography', {
-        url: '/photography',
-        templateUrl: 'views/photography.html',
-        controller: 'PhotographyCtrl'
-      })
-      .state('art.modal', {
-        url: '',
-        onEnter: function($stateParams, $modal) {
-          $modal.open({
-            templateUrl: 'views/modal.html',
-            controller: 'ModalCtrl',
-            backdrop: 'static'
-          });
-        }
-      });
-  });
+var ackushiw = angular.module('ackushiwApp', [
+  'ngCookies',
+  'ngMaterial',
+  'ngResource',
+  'ngSanitize',
+  'ui',
+  'ui.bootstrap',
+  'ui.router',
+  'akoenig.deckgrid',
+  'ngTouch',
+  'ngAnimate'
+]);
+ackushiw.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/alexander');
+  $stateProvider
+    .state('alexander', {
+      url: '/alexander',
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('acting', {
+      url: '/acting',
+      templateUrl: 'views/acting.html',
+      controller: 'ActingCtrl'
+    })
+    .state('art', {
+      url: '/art',
+      templateUrl: 'views/art.html',
+      controller: 'ArtCtrl'
+    })
+    .state('art.photography', {
+      url: '/photography',
+      templateUrl: 'views/photography.html',
+      controller: 'PhotographyCtrl'
+    })
+    .state('art.modal', {
+      url: '',
+      onEnter: function($stateParams, $modal) {
+        $modal.open({
+          templateUrl: 'views/modal.html',
+          controller: 'ModalCtrl',
+          backdrop: 'static'
+        });
+      }
+    });
+});
+ackushiw.run(['$rootScope', '$window',
+  function($rootScope, $window) {
+    $rootScope.deviceHeight = $window.innerHeight;
+    $rootScope.deviceWidth = $window.innerWidth;
+  }
+]);
